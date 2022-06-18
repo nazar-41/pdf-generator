10c133dd713a4b12d27caa61065dfd1977cc7004
Nazar <nazarwelkakayew41@gmail.com>
2022-06-18 11:30:46 +0500

    update

--- Sources/SwiftUIView.swift
+++ Sources/SwiftUIView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 @available(iOS 13.0, *)
 struct SwiftUIView: View {
     var body: some View {
-        Text("This is a swiftui view from the swift package")
+        VStack {
+            Text("This is a swiftui view from the swift package")
+            Text("SUCCESS")
+        }
     }
 }
