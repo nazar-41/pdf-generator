06fce5de3cf10fccaff574bbc8ace88090638c40
Nazar <nazarwelkakayew41@gmail.com>
2022-06-18 11:27:41 +0500

    update

---
+++ example/PDF-Generator-ExampleProject/PDF-Generator-ExampleProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PDF-Generator-ExampleProject
+//
+//  Created by Belli's MacBook on 18/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
