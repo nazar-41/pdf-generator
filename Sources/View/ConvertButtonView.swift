58c8838dc7f67996d379f52e278526db0245dc9a
Nazar <nazarwelkakayew41@gmail.com>
2022-06-20 10:56:49 +0500

    check

--- Sources/View/ConvertButtonView.swift
+++ Sources/View/ConvertButtonView.swift
@@ -6,25 +6,36 @@
 //
 
 import SwiftUI
+import Foundation
+import UIKit
 
 @available(iOS 13.0, *)
-public struct GeneratingButtonView: View {
-    public var buttonLabel: AnyView
-    public var convertingView: AnyView
+public struct GeneratingButtonView<Content: View>: View {
+//    public var buttonLabel: AnyView
+//    public var convertingView: AnyView
     public var showSheet: Binding<Bool>
+    
+    var buttonLabel: () -> Content
+    var convertingView: () -> Content
     
     @StateObject var viewModel = ShareSheetViewModel()
     
-    public init(showSheet: Binding<Bool>, buttonLabel: AnyView, convertingView: AnyView) {
+//    public init(showSheet: Binding<Bool>, buttonLabel: AnyView, convertingView: AnyView) {
+//        self.buttonLabel = buttonLabel
+//        self.convertingView = convertingView
+//        self.showSheet = showSheet
+//    }
+    public init(showSheet: Binding<Bool>, buttonLabel: @escaping () -> Content, convertingView: @escaping ()-> Content) {
         self.buttonLabel = buttonLabel
         self.convertingView = convertingView
         self.showSheet = showSheet
     }
+    
     public var body: some View {
         VStack {
             Button {
                 exportToPDF {
-                    convertingView
+                    //convertingView
                 } complition: { status, url in
                     if let url = url,status{
                         viewModel.pdfURL = url
@@ -34,7 +45,7 @@ public struct GeneratingButtonView: View {
                     }
                 }
             } label: {
-                buttonLabel
+               //buttonLabel  //buttonLabel
             }
         }
         .sheet(isPresented: showSheet) {
